288317d666f50f8eb3470cb6e0e9af3b84eef2e1
mmsdiego <mmsdiego@gmail.com>
2019-12-03 02:50:07 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CepFormComponent } from './components/cep-form/cep-form.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CepFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
